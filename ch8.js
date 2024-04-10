0d8c5200f967f567910e1a0d8f473223a3d8fd9a
lrng-object <152777550+lrng-object@users.noreply.github.com>
2024-04-10 14:23:26 +0200

    Add files via upload

---
+++ ch8.js
@@ -0,0 +1,11 @@
+let miLista = ["velma", "exploradora", "jane", "john", "harry"];
+miLista.forEach(elemento => console.log(elemento));
+
+let indice = 0;
+while(indice < miLista.length){
+    console.log(miLista[indice]);
+    ++indice;
+}
+
+const holaMundo = () => "Hola Mundo";
+console.log(holaMundo());
